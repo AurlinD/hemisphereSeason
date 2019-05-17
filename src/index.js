7d6cd2ae42de50b03fa8308ff195778f4974c099
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-05-16 17:57:21 -0700

    added spinner loading sign for when waiting for callback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   //not required by react
@@ -37,7 +38,7 @@ class App extends React.Component {
       //return <div>Latitude: {this.state.lat}</div>;
       return <SeasonDisplay lat={this.state.lat} />;
     } else {
-      return <div>Loading!</div>;
+      return <Spinner />;
     }
   }
 }
